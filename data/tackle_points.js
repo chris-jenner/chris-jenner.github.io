a82671ab2ef103e719bfba48bbfea2ce4e5eb0a7
Chris Jenner <chrisjenner@thefishingindex.com>
2021-02-07 16:24:41 +0000

    Update tackle_points.js

--- data/tackle_points.js
+++ data/tackle_points.js
@@ -1,13 +1,27 @@
 var tackle_points = {
 "type": "FeatureCollection",
-"name": "tackle_points",
+"name": "tackle_shops",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "name": "Premier Angling", "url": "http:\/\/premierangling.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.490175609022154, 51.419068903656928 ] } },
-{ "type": "Feature", "properties": { "id": 2, "name": "Veals Fishing Tackle", "url": "http:\/\/www.practicalfishingtackle.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.581562195200779, 51.45456142579792 ] } },
-{ "type": "Feature", "properties": { "id": 3, "name": "Bristol Angling Centre", "url": "http:\/\/www.bristolangling.com\/" }, "geometry": { "type": "Point", "coordinates": [ -2.598122242380094, 51.497929940527008 ] } },
-{ "type": "Feature", "properties": { "id": 4, "name": "J P Angling", "url": "http:\/\/bjpangling.runboard.com\/" }, "geometry": { "type": "Point", "coordinates": [ -2.506691394406992, 51.462312962201636 ] } },
-{ "type": "Feature", "properties": { "id": 5, "name": "Scott Tackle", "url": "http:\/\/www.scott-tackle.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.508073589516637, 51.477213795570634 ] } }
+{ "type": "Feature", "properties": { "id": 1, "name": "premier angling", "url": "http:\/\/premierangling.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.490175609022154, 51.419068903656928 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "bristol angling centre", "url": "http:\/\/www.bristolangling.com\/" }, "geometry": { "type": "Point", "coordinates": [ -2.598122242380094, 51.497929940527008 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "j p angling", "url": "http:\/\/bjpangling.runboard.com\/" }, "geometry": { "type": "Point", "coordinates": [ -2.506691394406992, 51.462312962201636 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "scott tackle", "url": "http:\/\/www.scott-tackle.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.508073589516637, 51.477213795570634 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "deggys tackle shop", "url": null }, "geometry": { "type": "Point", "coordinates": [ -2.997535503059071, 53.041718690982137 ] } },
+{ "type": "Feature", "properties": { "id": 6, "name": "deeside tackle", "url": null }, "geometry": { "type": "Point", "coordinates": [ -3.037607012194784, 53.211328142476177 ] } },
+{ "type": "Feature", "properties": { "id": 7, "name": "the tackle store", "url": "https:\/\/www.thetacklestore.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.92984277038457, 53.193921111552292 ] } },
+{ "type": "Feature", "properties": { "id": 8, "name": "chester tackle locker", "url": "http:\/\/tacklelocker.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -2.862808401163549, 53.192440739230044 ] } },
+{ "type": "Feature", "properties": { "id": 9, "name": "harrys tackle and bait", "url": "https:\/\/fishingtacklenorthwales.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -3.482172328356031, 53.317107429938417 ] } },
+{ "type": "Feature", "properties": { "id": 10, "name": "foxons tackle", "url": "https:\/\/foxons.com\/" }, "geometry": { "type": "Point", "coordinates": [ -3.448413276374206, 53.255017257969925 ] } },
+{ "type": "Feature", "properties": { "id": 11, "name": "pensarn baits", "url": "https:\/\/www.pensarnbaits.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -3.580254084384018, 53.294271235864464 ] } },
+{ "type": "Feature", "properties": { "id": 12, "name": "geoffs tackle and bait", "url": "http:\/\/geoffstackleandbait.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -3.498050319496286, 53.316005674365051 ] } },
+{ "type": "Feature", "properties": { "id": 13, "name": "flintshire tackle ltd", "url": null }, "geometry": { "type": "Point", "coordinates": [ -3.130659436419733, 53.247806106686731 ] } },
+{ "type": "Feature", "properties": { "id": 14, "name": "rhos tackle and bait", "url": null }, "geometry": { "type": "Point", "coordinates": [ -3.739057292322916, 53.309215438171229 ] } },
+{ "type": "Feature", "properties": { "id": 15, "name": "anglesey bait centre", "url": null }, "geometry": { "type": "Point", "coordinates": [ -4.103401320535356, 53.255951308472632 ] } },
+{ "type": "Feature", "properties": { "id": 16, "name": "caernarfon angling", "url": "https:\/\/caernarfon-angling.business.site\/" }, "geometry": { "type": "Point", "coordinates": [ -4.271069091967594, 53.139079906304332 ] } },
+{ "type": "Feature", "properties": { "id": 17, "name": "llyn-y-gors tackle", "url": "https:\/\/www.llynygors.co.uk\/" }, "geometry": { "type": "Point", "coordinates": [ -4.134745029588597, 53.252362123695001 ] } },
+{ "type": "Feature", "properties": { "id": 18, "name": "criccieth tackle box", "url": "https:\/\/www.cricciethtacklebox.shop\/" }, "geometry": { "type": "Point", "coordinates": [ -4.23371955641699, 52.919728048532427 ] } },
+{ "type": "Feature", "properties": { "id": 19, "name": "menai kayak angling", "url": "http:\/\/angelseybaitandlures.co.uk\/?utm_source=gmb&utm_medium=referral" }, "geometry": { "type": "Point", "coordinates": [ -4.161922260028414, 53.225094369830799 ] } }
 ]
 };
 
