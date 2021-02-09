4d88d47d983dc468d80d7bd3bcaef5e5ffaac23a
Chris Jenner <chrisjenner@thefishingindex.com>
2021-02-09 21:58:36 +0000

    Update century-ponds.js

--- century-ponds.js
+++ century-ponds.js
@@ -79,17 +79,6 @@ const lakes = L.geoJSON(fisheries_point, {
   pointToLayer: function(geoJsonPoint, latlng) {
     return L.marker(latlng, {icon: lakeicon});
      },
-  style: function(feature) {
-    return {
-      color: feature.properties.color
-    };
-  },
-  filter: (feature) => {
-    if (checkboxStates.length == 0) return true;
-    return checkboxStates.every(function(element) {
-      return feature.properties[element.name];
-    });
-  }
 }).bindPopup(function(layer) {
   let cap_name = layer.feature.properties.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
   return `<p>${cap_name}</p><a href="http://${layer.feature.properties.tfi_url}" target="_blank">View<a>`;
@@ -98,13 +87,7 @@ const lakes = L.geoJSON(fisheries_point, {
 const rivers = L.geoJSON(fisheries_point, {
   pointToLayer: function(geoJsonPoint, latlng) {
     return L.marker(latlng, {icon: lakeicon});
-     },
-  filter: (feature) => {
-    if (checkboxStates.length == 0) return true;
-    return checkboxStates.every(function(element) {
-      return feature.properties[element.name];
-    });
-  },
+     },  
 }).bindPopup(function(layer) {
 	let cap_name = layer.feature.properties.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
   return `<p>${cap_name}</p><a href="https://${layer.feature.properties.tfi_url}" target="_blank">View<a>`;
