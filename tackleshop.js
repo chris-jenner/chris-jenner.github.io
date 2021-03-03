cda0ef55530ab09b87bdaea5be725dc1e2de5827
Chris Jenner <chrisjenner@thefishingindex.com>
2021-03-03 21:33:37 +0000

    Update tackleshop.js

--- tackleshop.js
+++ tackleshop.js
@@ -70,7 +70,13 @@ var shopicon = L.icon({
     pointToLayer: function(geoJsonPoint, latlng) {
       return L.marker(latlng, {icon: shopicon});
        },
-    }).bindPopup(function(layer) {
-    let cap_name = layer.feature.properties.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-    return `<p>${cap_name}</p><a href="http://${layer.feature.properties.url}" target="_blank">View<a>`;
-  }).addTo(map);
+     }).bindPopup(function (layer) {
+             let cap_name = layer.feature.properties.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter
+                 .toUpperCase());
+             if (layer.feature.properties.url != null) {
+                 return `<p>${cap_name}</p><a href="http://${layer.feature.properties.url}" target="_blank">View<a>`;
+              } else {
+                 return `<p>${cap_name}</p>`;
+              }
+  
+         }).addTo(map);
