7d0d23ad759e0a4754e14661aebd289b34556798
Chris Jenner <chrisjenner@thefishingindex.com>
2021-10-24 16:24:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ L.control.zoom({
 
 var lakeicon = L.icon({
   iconUrl: 'images/fisheries_50px.png',
-  iconSize: [40, 49],
+  iconSize: [30, 39],
   iconAnchor: [20,20],
   popupAnchor: [3,-20]
   });
